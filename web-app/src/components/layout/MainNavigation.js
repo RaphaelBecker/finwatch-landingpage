b00b5bce6dd1bbfed116d94b92f09ffe49ae8181
RaphaelBecker <raphael.becker.private@outlook.de>
2022-09-26 17:11:30 +0200

    added css nav bar

--- web-app/src/components/layout/MainNavigation.js
+++ web-app/src/components/layout/MainNavigation.js
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
-    <header>
-      <div>LOGO</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>LOGO</div>
       <nav>
         <ul>
           <li>
-            <Link to="/">Landing page</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/education">Education page</Link>
+            <Link to="/education">Education</Link>
           </li>
           <li>
-            <Link to="/signup">Signup page</Link>
+            <Link to="/signup">Signup</Link>
           </li>
         </ul>
       </nav>
--- web-app/src/pages/Landing-page.js
+++ web-app/src/pages/Landing-page.js
@@ -1,8 +1,24 @@
+import Card from "../components/ui/Card.js";
+
 function LandingPage() {
   return (
-    <section>
-      <h1>This is LandingPage</h1>
-    </section>
+    <>
+      <section>
+        <h1>This is LandingPage</h1>
+      </section>
+
+      <Card>
+        <div className="container">
+          <h2>This is a card</h2>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="container">
+          <h2>This is a card</h2>
+        </div>
+      </Card>
+    </>
   );
 }
 export default LandingPage;
